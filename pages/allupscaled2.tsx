5586350a218e79b4a6cd269381a78d6b531ec3af
marius <nedzen.11@gmail.com>
2023-03-31 17:18:07 +0200

    added ethereal

---
+++ pages/allupscaled2.tsx
@@ -0,0 +1,58 @@
+// import { format, parseISO } from 'date-fns';
+// import { GetStaticProps } from 'next';
+
+import React from 'react';
+import Layout from '../components/Layout';
+// import { getAllPosts } from '../lib/api';
+// import { PostType } from '../types/post';
+// import Image from 'next/image';
+import Generation from '../data/replicate_lg_3.json';
+// import NFT from '../components/NFT';
+
+// const blurIMG = 'https://www.mariusnedelcu.com/images/kitty.jpeg';
+
+export const NFTpage = (): JSX.Element => {
+  return (
+    <Layout>
+      <main className="NFT">
+        <ul className="articleList">
+          {Generation.map((el, index: number) => {
+            return (
+              <li key={index}>
+                <ul className="tillandsias">
+                  <li>
+                    {/* <Image
+                      blurDataURL={blurIMG}
+                      placeholder="blur"
+                      alt={el.id}
+                      src={el.href}
+                      width={el.width}
+                      height={el.height}
+                      priority
+                      layout="responsive"
+                    /> */}
+                  </li>
+                  <img src={el.href} />
+                  <li>{el.id}</li>
+                  {/* <li>{el.width}</li>
+                  <li>{el.height}</li>
+                  <li>{el.guidance_scale}</li>
+                  <li>{el.inference_steps}</li>
+                  <li>{el.url}</li> */}
+                  <li>{el.href}</li>
+                  <li>
+                    <a download="out.png" href={el.href} title="ImageName">
+                      {el.id}
+                    </a>
+                  </li>
+                </ul>
+              </li>
+            );
+          }).reverse()}
+        </ul>
+      </main>
+    </Layout>
+  );
+};
+
+export default NFTpage;
